a4de5fe5bab3146105289973efe4836d4b548882
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-10 07:39:56 -0300

    Update testimony component styles

--- src/components/testimony/name.tsx
+++ src/components/testimony/name.tsx
@@ -3,5 +3,5 @@ import { FC, PropsWithChildren } from 'react';
 export type NameProps = PropsWithChildren<{}>;
 
 export const Name: FC<NameProps> = ({ children }) => {
-  return <h6 className='text-gray-500 text-base mx-4 mt-4'>{children}</h6>;
+  return <h6 className='text-gray-500 text-base'>{children}</h6>;
 };
