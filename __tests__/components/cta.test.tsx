c609592a42b5848b98554623d007bb811606ecea
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-15 06:05:43 -0300

    Update CTAButton component to use anchor tag instead of button

--- __tests__/components/cta.test.tsx
+++ __tests__/components/cta.test.tsx
@@ -4,25 +4,25 @@ import { CTAButton } from '@/components/cta';
 
 describe('CTAButton', () => {
   it('renders a button', () => {
-    render(<CTAButton>Test</CTAButton>);
+    render(<CTAButton href='/'>Test</CTAButton>);
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toBeInTheDocument();
   });
 
   it('should render with children content', () => {
-    render(<CTAButton>Test</CTAButton>);
+    render(<CTAButton href='/'>Test</CTAButton>);
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveTextContent('Test');
   });
 
   it('should render with properly style', () => {
-    render(<CTAButton>Test</CTAButton>);
+    render(<CTAButton href='/'>Test</CTAButton>);
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveClass(
       'px-6 py-3 font-bold rounded-md text-lg bg-off-white text-charcoal-grey-500'
@@ -32,9 +32,13 @@ describe('CTAButton', () => {
   });
 
   it('should render with properly style when buttonType is primary', () => {
-    render(<CTAButton buttonType='primary'>Test</CTAButton>);
+    render(
+      <CTAButton href='/' buttonType='primary'>
+        Test
+      </CTAButton>
+    );
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveClass(
       'px-6 py-3 font-bold rounded-md text-lg bg-deep-blue text-off-white'
@@ -42,9 +46,13 @@ describe('CTAButton', () => {
   });
 
   it('should render with properly style when buttonType is secondary', () => {
-    render(<CTAButton buttonType='secondary'>Test</CTAButton>);
+    render(
+      <CTAButton href='/' buttonType='secondary'>
+        Test
+      </CTAButton>
+    );
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveClass(
       'px-6 py-3 font-bold rounded-md text-lg bg-gold text-deep-blue'
@@ -52,29 +60,37 @@ describe('CTAButton', () => {
   });
 
   it('should render with custom class', () => {
-    render(<CTAButton className='custom-class'>Test</CTAButton>);
+    render(
+      <CTAButton href='/' className='custom-class'>
+        Test
+      </CTAButton>
+    );
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveClass('custom-class');
   });
 
   it('should render with custom class when buttonType is primary', () => {
     render(
-      <CTAButton className='custom-class' buttonType='primary'>
+      <CTAButton href='/' className='custom-class' buttonType='primary'>
         Test
       </CTAButton>
     );
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveClass('bg-deep-blue text-off-white custom-class');
   });
 
   it('fluid button should take full width', () => {
-    render(<CTAButton fluid>Test</CTAButton>);
+    render(
+      <CTAButton href='/' fluid>
+        Test
+      </CTAButton>
+    );
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     expect(button).toHaveClass('w-full');
   });
@@ -82,9 +98,13 @@ describe('CTAButton', () => {
   it('should run onClick function', () => {
     const onClick = jest.fn();
 
-    render(<CTAButton onClick={onClick}>Test</CTAButton>);
+    render(
+      <CTAButton href='/' onClick={onClick}>
+        Test
+      </CTAButton>
+    );
 
-    const button = screen.getByRole('button');
+    const button = screen.getByRole('link');
 
     button.click();
 
@@ -92,7 +112,7 @@ describe('CTAButton', () => {
   });
 
   it('should match snapshot', () => {
-    const { container } = render(<CTAButton>Test</CTAButton>);
+    const { container } = render(<CTAButton href='/'>Test</CTAButton>);
 
     expect(container).toMatchSnapshot();
   });
