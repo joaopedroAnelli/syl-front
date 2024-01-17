963762b166e61e79a8d6bdcfc41e430699d5c2a0
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-17 07:40:35 -0300

    Add new types to form module

--- src/app/form/types.d.ts
+++ src/app/form/types.d.ts
@@ -1,7 +1,45 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export type FormTexts = {
   location: string;
   details: string;
   contact: string;
   nextButton: string;
   backButton: string;
+};
+
+export type FormContact = {
+  name: string;
+  email: string;
+  phone?: string;
+};
+
+export type FormSchema = {
+  address: string;
+  city: string;
+  state: string;
+  zip?: string;
+  size: number;
+  sizeUnit: string;
+  offerPrice: number;
+  details?: string;
+  contact: FormContact;
+};
+
+export type ValidatorResponse =
+  | {
+      isValid: boolean;
+      errorMessage?: string;
+    }
+  | {
+      isValid: false;
+      errorMessage: string;
+    };
+
+export type FormContext = {
+  data: FormSchema;
+  setData: Dispatch<SetStateAction<FormSchema>>;
+  isNextButtonDisabled: boolean;
+  setIsNextButtonDisabled: Dispatch<SetStateAction<boolean>>;
+  pageValidatorRef: React.MutableRefObject<() => ValidatorResponse>;
 };
