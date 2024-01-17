007abd44f2c0aef80d87a101e6586f104d17243e
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-17 07:40:44 -0300

    Refactor FlowButton component to use Button component and add onClick functionality

--- src/components/flow-button/index.tsx
+++ src/components/flow-button/index.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 import { FC, PropsWithChildren } from 'react';
-import { LinkButton, LinkButtonProps } from '@/components/link-button';
 import { FlowPage } from '@/types/flow';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { Button, ButtonProps } from '@/components/button';
 
 type FlowButtonBaseProps = {
   flow: FlowPage[];
   isBack?: boolean;
+  onClick?: () => void;
 };
 
-type LinkButtonPropsWithoutHref = Omit<LinkButtonProps, 'href'>;
+type LinkButtonPropsWithoutHref = ButtonProps;
 
 type FlowButtonProps = PropsWithChildren<
   FlowButtonBaseProps & LinkButtonPropsWithoutHref
@@ -20,17 +21,33 @@ export const FlowButton: FC<FlowButtonProps> = ({
   flow,
   isBack = false,
   children,
+  onClick,
   ...props
 }) => {
   const pathname = usePathname();
+
+  const { prefetch } = useRouter();
 
   const index = flow.findIndex((f) => f.route.includes(pathname));
 
   const next = isBack ? flow[index - 1] : flow[index + 1];
 
+  const handleClick = () => {
+    if (onClick) {
+      try {
+        onClick();
+      } catch (error) {
+        console.error(error);
+        return;
+      }
+    }
+
+    prefetch(next ? next.route : '/');
+  };
+
   return (
-    <LinkButton {...props} href={next ? next.route : '/'}>
+    <Button {...props} onClick={handleClick}>
       {children}
-    </LinkButton>
+    </Button>
   );
 };
