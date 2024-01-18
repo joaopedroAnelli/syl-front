a3bdee97d6f43917c37efe0524a590d2c4df501b
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-18 08:38:05 -0300

    Refactor requestCMS function to handle error responses

--- src/utils/requestCMS.ts
+++ src/utils/requestCMS.ts
@@ -1,9 +1,16 @@
 const requestCMS = async <ResponseDTO = any>(
-  url: string
+  url: string,
+  options?: RequestInit
 ): Promise<ResponseDTO> => {
   const res = await fetch(`http://localhost:3000/api/cms/${url}`, {
+    ...options,
     credentials: 'same-origin',
   });
+
+  if (!res.ok) {
+    console.error({ url, options, res });
+    throw new Error(res.statusText);
+  }
 
   return res.json();
 };
