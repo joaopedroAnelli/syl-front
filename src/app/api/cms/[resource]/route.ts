918f88ff66226a54ddbb8a087a6e6301124651bb
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-18 08:38:12 -0300

    Add POST method to API route

--- src/app/api/cms/[resource]/route.ts
+++ src/app/api/cms/[resource]/route.ts
@@ -25,3 +25,26 @@ export async function GET(req: NextRequest, context: { params: GetParams }) {
 
   return new Response(JSON.stringify(cmsResponse));
 }
+
+export async function POST(req: NextRequest, context: { params: GetParams }) {
+  const { params } = context;
+
+  const { resource } = params;
+
+  const body = await req.json();
+
+  const jsonBody = JSON.stringify(body);
+
+  const res = await fetch(`${process.env.CMS_HOST}/api/${resource}`, {
+    method: 'POST',
+    body: jsonBody,
+    headers: {
+      Authorization: `Bearer ${process.env.CMS_API_KEY}`,
+      'Content-Type': 'application/json; charset=utf8',
+    },
+  });
+
+  const cmsResponse = await res.json();
+
+  return new Response(JSON.stringify(cmsResponse));
+}
