62e7ecd9082568235dcf53f7d87bb599c2fc236f
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-18 23:06:05 -0300

    Add client-side check for address in useCheckLocation hook

--- src/app/form/hooks/useCheckLocation.ts
+++ src/app/form/hooks/useCheckLocation.ts
@@ -1,4 +1,6 @@
-import { useContext } from 'react';
+'use client';
+
+import { useContext, useEffect } from 'react';
 import { FormContext } from '../context';
 import { useRouter } from 'next/navigation';
 
@@ -6,7 +8,9 @@ export default function useCheckLocation() {
   const { data } = useContext(FormContext);
   const router = useRouter();
 
-  if (!data.address) {
-    router.push('/form/location');
-  }
+  useEffect(() => {
+    if (!data.address) {
+      router.push('/form/location');
+    }
+  }, [data.address, router]);
 }
