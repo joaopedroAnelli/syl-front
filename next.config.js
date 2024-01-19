6da521218af1c98000760c6aae96f9a9a6875366
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-18 22:55:51 -0300

    Update .env.production and CMSImage component

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
         pathname: '/uploads/**',
         port: '1337',
       },
+      {
+        hostname: 'legendary-festival-bf97c4341c.media.strapiapp.com',
+        protocol: 'https',
+      },
     ],
   },
   compiler: {
--- src/components/cms-image/index.tsx
+++ src/components/cms-image/index.tsx
@@ -7,7 +7,7 @@ export type CMSImageProps = ImageProps & {};
 const CMSImage: FC<ImageProps> = ({ src, alt, ...rest }) => {
   const cmsUri = process.env.CMS_HOST || '';
 
-  return <Image src={cmsUri + src} alt={alt} {...rest} />;
+  return <Image src={src} alt={alt} {...rest} />;
 };
 
 export default CMSImage;
