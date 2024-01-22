f0da72af4bc1c3e970e62b39e8f3020dec6e10ee
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-01-22 15:31:40 -0300

    Fix CMSImage src path

--- src/components/cms-image/index.tsx
+++ src/components/cms-image/index.tsx
@@ -7,7 +7,9 @@ export type CMSImageProps = ImageProps & {};
 const CMSImage: FC<ImageProps> = ({ src, alt, ...rest }) => {
   const cmsUri = process.env.CMS_HOST || '';
 
-  return <Image src={src} alt={alt} {...rest} />;
+  const finalSrc = src.toString().startsWith('/') ? `${cmsUri}${src}` : src;
+
+  return <Image src={finalSrc} alt={alt} {...rest} />;
 };
 
 export default CMSImage;
