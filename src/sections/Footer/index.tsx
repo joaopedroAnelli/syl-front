d4e8cddbabc6517c1b0d48a5836b648d4614a3cb
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-02-04 19:27:37 -0300

    Refactor Footer component to display a static name and simplified copyright message

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -1,23 +1,17 @@
-import { fetchPageData } from '@/utils/fetchPageData';
 import { FC } from 'react';
-import { Texts } from './types';
 
 export type FooterProps = {};
 
 export const Footer: FC<FooterProps> = async ({}) => {
-  const pageData = await fetchPageData<Texts>('home');
-
-  const { texts } = pageData;
-
   const year = new Date().getFullYear();
+
   return (
     <div className='p-6 flex justify-between bg-deep-blue-600 text-slate-500 w-full'>
-      <p>{texts.footer.name}</p>
+      <p>Sell Your Lot</p>
 
       <p>
-        {texts.footer.showCopySign ? '\u00A9 ' : ''}
-        {texts.footer.rights}
-        {texts.footer.showYearAfterRights ? ' ' + year : ''}
+        {'\u00A9 '}
+        All rights reserved {year}
       </p>
     </div>
   );
