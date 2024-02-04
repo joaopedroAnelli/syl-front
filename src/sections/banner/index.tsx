b62f68fa4550aa71c9f030f31fc85a150f2c62f1
joaopedroAnelli <joaopedroanelli99@gmail.com>
2024-02-04 18:16:21 -0300

    Refactor banner section to use Next.js Image component

--- src/sections/banner/index.tsx
+++ src/sections/banner/index.tsx
@@ -1,44 +1,32 @@
 import { Header } from '@/components/header';
 import { FC } from 'react';
-import CMSImage from '@/components/cms-image';
 import { Subtitle } from '@/components/subtitle';
-import { fetchPageData } from '@/utils/fetchPageData';
-import { Texts } from './types';
+import Image from 'next/image';
+import BannerPic from '~/public/images/banner.webp';
 
 export type BannerProps = {};
 
 export const Banner: FC<BannerProps> = async ({}) => {
-  const pageData = await fetchPageData<Texts>('home');
-
-  const { texts, images } = pageData;
-
-  const bannerImage = images?.data.find(
-    (image) => image.attributes.name === 'banner'
-  );
-
   return (
     <section className='flex flex-col h-[50vh] relative p-6 md:p-12'>
-      {bannerImage && (
-        <CMSImage
-          alt={bannerImage.attributes.alternativeText}
-          src={bannerImage.attributes.url}
-          fill
-          sizes='100vw'
-          quality={100}
-          placeholder='blur'
-          blurDataURL={bannerImage.attributes.formats.thumbnail.url}
-          style={{ objectFit: 'cover' }}
-        />
-      )}
+      <Image
+        alt='A pretty view of a land with a sunrise'
+        src={BannerPic}
+        fill
+        sizes='100vw'
+        quality={100}
+        placeholder='blur'
+        style={{ objectFit: 'cover' }}
+      />
 
       <div className='z-10 flex items-center justify-center backdrop-blur rounded-md bg-deep-blue/20 p-6 md:p-12 h-full'>
         <div className='text-center'>
           <Header as='h2' size='md' className='z-10 mb-1'>
-            {texts.banner.title}
+            Unlock the True Potential of Your Land
           </Header>
 
           <Subtitle className='z-10 text-white'>
-            {texts.banner.subtitle}
+            Transform Acres into Assets.
           </Subtitle>
         </div>
       </div>
